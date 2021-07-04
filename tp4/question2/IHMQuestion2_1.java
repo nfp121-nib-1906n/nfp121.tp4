958e4701210e805c7d94aad1e928599b48d3314a
MariapBouAoun <73455465+MariaPBouAoun3@users.noreply.github.com>
2021-07-04 20:12:38 +0300

    Update IHMQuestion2_1.java

--- tp4/question2/IHMQuestion2_1.java
+++ tp4/question2/IHMQuestion2_1.java
@@ -27,12 +27,24 @@ public class IHMQuestion2_1 extends JFrame {
         setLocation(100,100);
         pack();show();
 
-        // à compléter
+    
         // le bouton A a 3 observateurs jbo1, jbo2 et jbo3
 
         // le bouton B a 2 observateurs jbo1 et jbo2
 
         // le bouton C a 1 observateur jbo1
+        jbo1 = new JButtonObserver("jbo1", contenu);
+        jbo2 = new JButtonObserver("jbo2", contenu);
+        jbo3 = new JButtonObserver("jbo3", contenu);
+        
+      
+        boutonA.addActionListener(jbo1);
+        boutonA.addActionListener(jbo2);
+        boutonA.addActionListener(jbo3);
+        boutonB.addActionListener(jbo1);
+        boutonB.addActionListener(jbo2);
+        boutonC.addActionListener(jbo1);
+
 
       
     }
